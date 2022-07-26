1122853e1f56ca46f2e4ac870907f29e88f5d645
xaldarof <temuruz2003@gmail.com>
2022-07-26 21:59:56 +0500

    auth

--- app/src/main/java/com/example/quizapp/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/quizapp/ui/fragments/LoginFragment.kt
@@ -1,12 +1,16 @@
 package com.example.quizapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.quizapp.databinding.LoginFragmentLayoutBinding
+import kotlinx.coroutines.launch
 
 /**
  * @Author: Temur
@@ -16,6 +20,7 @@ import com.example.quizapp.databinding.LoginFragmentLayoutBinding
 class LoginFragment : Fragment() {
 
     private lateinit var binding: LoginFragmentLayoutBinding
+    private val liveData = MutableLiveData<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,8 +33,18 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            liveData.observe(viewLifecycleOwner) {
+                binding.wordsTv.text = it.toString()
+            }
+        }
+
+        var count = 0
         binding.registerTv.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
+            count++
+            liveData.value = count
+//            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
         }
     }
 }
